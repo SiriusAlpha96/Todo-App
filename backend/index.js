31833c67c122fa36a2d42b1fc1ad56952354bea2
SiriusAlpha96 <shreyama9670@gmail.com>
2024-04-13 01:13:32 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const { createTodo } = require("./type");
 const app = express();
 
 app.use(express.json());
 
 
 app.post("/todo", function(req,res){
-
+    const createPayLoad = req.body;
+    const parsedPayLoad = createTodo.safeParse(createPayLoad)
+    if(!parsedPayLoad.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+    }
 })
 
 app.get("/todos", function(req,res){
@@ -13,7 +21,14 @@ app.get("/todos", function(req,res){
 })
 
 app.post("/completed", function(req,res){
-
+     const updatePayLoad = req.body;
+     const parsedPayLoad = updateTodo.safeParse(updatePayLoad);
+     if(!parsedPayLoad.success){
+        res.status(411).json({
+            msg: "You sent the wrong inputs",
+        })
+        return;
+     }
 })
 
 app.listen(3000);
