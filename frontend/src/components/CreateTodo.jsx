cda495efda867ec007cbe94bd551180a86554571
SiriusAlpha96 <shreyama9670@gmail.com>
2024-04-14 00:43:23 +0530

    updated commit

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,6 +1,9 @@
-
+import { useState } from "react";
 
 export function CreateTodo(){
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("")
+
     return <div>
         <input style={{
             padding:10,
@@ -14,6 +17,20 @@ export function CreateTodo(){
         <button style={{
             padding:10,
             margin:10
+        }} onClick={()=>{
+            fetch("http:localhost:3000/todos",{
+                method: "POST",
+                body:{
+                    title: "",
+                    description: ""
+
+                }
+            })
+                .then(async function(res){
+                    const json = await res.json();
+                    alert("Todo added")
+                })
+            
         }}>Add Todo</button>
     </div>
 }
